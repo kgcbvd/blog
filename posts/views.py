84335dc3e24eaeae1b7bdd211a504b3657590227
kgcbvd <vdcbgn@gmail.com>
2016-06-09 16:18:10 +0400

    update view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .forms import PostForm
@@ -8,20 +8,14 @@ def post_create(request):
     form = PostForm(request.POST or None)
     if form.is_valid():
         instance = form.save(commit=False)
-        print form.cleaned_data.get("title")
         instance.save()
-    """
-    if request.method == "POST":
-        print request.POST.get("content")
-        print request.POST.get("title")
-        #Post.objects.create(title=title, content=content)"""
+        return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         "form": form,
     }
     return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
-    #instance = Post.objects.get(id=1)
     instance = get_object_or_404(Post, id=id)
     context = {
         "title": instance.title,
@@ -35,19 +29,21 @@ def post_list(request):
         "object_list": queryset,
         "title": "List"
     }
-    """
-    if request.user.is_authenticated():
-        context = {
-            "title": "My User List"
-        }
-    else:
-        context = {
-            "title": "List"
-        }"""
     return render(request, "index.html", context)
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 def post_delete(request):
     return HttpResponse("<h1>Delete</h1>")
